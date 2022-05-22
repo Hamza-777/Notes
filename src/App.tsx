e107521696916f01e3e6a5d1a1dbdf5087c627a8
Hamza <hamzararrani77@gmail.com>
2022-05-22 21:21:42 +0530

    Fix: Fixed the route name for home

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
         <Route path='/' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
         <Route
-          path='/signup'
+          path='/home'
           element={
             <PrivateRoute>
               <Home />
